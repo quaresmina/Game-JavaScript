b885f1afa5d198d861189a68d5b2d4d1df327362
quaresmina <arielquaresma90@gmail.com>
2022-07-19 21:23:58 -0300

    Implementação do botão de reiniciar/reload na pagina

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const pacman = document.querySelector(".pacman")
 const ghost = document.querySelector(".ghost")
 
+// Declaração da função jump - o pulo
+
 const jump = () => {
     pacman.classList.add("jump-pacman");
     setTimeout(() => {
         pacman.classList.remove("jump-pacman");
     }, 500)
 }
+
+//Intervalo para criação do evento de colisão
 
 const loopGame = setInterval(() => {
     const ghostPosition = ghost.offsetLeft;
@@ -25,21 +29,25 @@ const loopGame = setInterval(() => {
         pacman.style.width = "105px";
         pacman.style.marginLeft = "45px";
 
+        const refresh = document.querySelector(".refresh");
+        const imgStart = document.querySelector(".imgStart")
+        console.log(imgStart)
+        refresh.addEventListener("click", refreshGame);
+        imgStart.removeAttribute("hidden");
+
     clearInterval(loopGame);
 }
     
 }, 10);
 
+//Chamamento da função jump, sempre que os eventos ocorrerem
+
 document.addEventListener("click", jump);
-
-const refreshButton = document.querySelector('.refresh')
-
-
-//refreshButton.addEventListener('click', refreshGame())
-
-//function refreshGame(){
-   // window.location.reload();
-
-//}
-
 document.addEventListener("keydown", jump);
+
+
+// Botão de reiniciar
+
+function refreshGame(){
+    window.location.reload();
+}
